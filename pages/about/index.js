1bb59f20f4253ee8f01f81bcc48a673d7e7e99c3
alannurul18 <alannurul18@gmail.com>
2022-03-15 15:31:38 +0700

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function About() {
+  return (
+    <div className="ada">
+      <h1 className="text-3xl font-bold underline">
+        About
+      </h1>
+    </div>
+  )
+}
